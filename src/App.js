4d1b2f91340fef7d6eff3ba2916221a52672d0d0
Alexander Morozov <alexander.morozov@postindustria.com>
2017-03-09 00:45:44 +0200

    Task-3 done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Post from './Post';
 import PostForm from './PostForm';
 
+const posts = [
+    { id: 1, name: 'Тест 1', description: 'Fugit eligendi dolorum numquam dignissimos velit.'},
+    { id: 2, name: 'Тест 2', description: 'Fugit eligendi dolorum dignissimos velit.'},
+    { id: 3, name: 'Тест 3', description: 'Fugit dolorum numquam dignissimos velit.'}
+];
+
 export default class App extends React.Component {
     render() {
         return (
                 <div>
-                <Post />
+                <Post news={posts}/>
                 <PostForm />
                 </div>
         );
